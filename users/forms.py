1aefad516988def6379f91aa1ab3f1405ecab0a5
Sachin Sharma <vsharma3973@gmail.com>
2021-05-20 04:01:23 +0530

    Added new feature in profile and some UI fixes.

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserResgisterForm(UserCreationForm):
@@ -12,3 +13,20 @@ class UserResgisterForm(UserCreationForm):
         model = User
         fields = ['first_name', 'last_name', 'username',
                   'email', 'password1', 'password2']
+
+
+class UserUpdateFrom(forms.ModelForm):
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=200)
+    last_name = forms.CharField(max_length=200)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username',
+                  'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserResgisterForm
+from .forms import UserResgisterForm, UserUpdateFrom, ProfileUpdateForm
 
 
 def register(request):
@@ -9,9 +9,9 @@ def register(request):
         form = UserResgisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
+            username = form.cleaned_data.get('first_name')
             messages.success(
-                request, f'{username} Your account has been created!, you can now able to login')
+                request, f'{username}, your account has been created!, you can now able to login')
             return redirect('login')
     else:
         form = UserResgisterForm()
@@ -20,4 +20,24 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateFrom(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            username = u_form.cleaned_data.get('first_name')
+            messages.success(
+                request, f'{username}, your profile has been update!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateFrom(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
