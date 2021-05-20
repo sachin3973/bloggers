ca5928023b7cecebc4ced779ef5ad262ab1d7c43
Sachin Sharma <vsharma3973@gmail.com>
2021-05-20 22:14:41 +0530

    Added the CRUD functionality.

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from PIL import Image
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -13,11 +13,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    def save(self):
-        super().save()
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
