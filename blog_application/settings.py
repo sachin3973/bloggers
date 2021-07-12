b7edb72dfac17137b36947e6ee9196e83ead4a59
Sachin Sharma <vsharma3973@gmail.com>
2021-07-12 21:13:08 +0530

    Added django-heroku

--- blog_application/settings.py
+++ blog_application/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -157,6 +158,8 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_REGION_NAME = "us-east-2"
 AWS_S3_SIGNATURE_VERSION = "s3v4"
+
+django_heroku.settings(locals())
 
 
 CKEDITOR_CONFIGS = {
