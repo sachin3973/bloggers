53adf37ddeda5436fe3dcf2b4c103f9a7bd9eaad
Sachin Sharma <vsharma3973@gmail.com>
2021-07-12 20:50:07 +0530

    Updated settings

--- blog_application/settings.py
+++ blog_application/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dmcub7tz8is-3*k*$-dd$bqm3trllv)wuj@6@zg5u%e6pg-z4-'
+# SECRET_KEY = 'django-insecure-dmcub7tz8is-3*k*$-dd$bqm3trllv)wuj@6@zg5u%e6pg-z4-'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['deverseblog.herokuapp.com']
 
 
 # Application definition
